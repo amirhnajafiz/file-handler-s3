1f6cab16349e89d45731639697838b7fe80de686
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-08 15:51:17 +0430

    tracing added

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
 	go.opentelemetry.io/otel v1.7.0 // indirect
+	go.opentelemetry.io/otel/exporters/jaeger v1.7.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.7.0 // indirect
 	go.opentelemetry.io/otel/trace v1.7.0 // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
--- internal/telemetry/trace/trace.go
+++ internal/telemetry/trace/trace.go
@@ -1,1 +1,56 @@
 package trace
+
+import (
+	"log"
+
+	"github.com/amirhnajafiz/hls/internal/telemetry/config"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdkTrace "go.opentelemetry.io/otel/sdk/trace"
+	semConv "go.opentelemetry.io/otel/semconv/v1.7.0"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func New(cfg config.Trace) trace.Tracer {
+	if !cfg.Enabled {
+		return trace.NewNoopTracerProvider().Tracer("hls/client")
+	}
+
+	exporter, err := jaeger.New(
+		jaeger.WithAgentEndpoint(jaeger.WithAgentHost(cfg.Agent.Host), jaeger.WithAgentPort(cfg.Agent.Port)),
+	)
+	if err != nil {
+		log.Fatalf("failed to initialize export pipeline: %v", err)
+	}
+
+	res, err := resource.Merge(
+		resource.Default(),
+		resource.NewSchemaless(
+			semConv.ServiceNamespaceKey.String("snapp"),
+			semConv.ServiceNameKey.String("mqtt-blackbox-exporter"),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	bsp := sdkTrace.NewBatchSpanProcessor(exporter)
+	tp := sdkTrace.NewTracerProvider(
+		sdkTrace.WithSampler(sdkTrace.ParentBased(sdkTrace.TraceIDRatioBased(cfg.Ratio))),
+		sdkTrace.WithSpanProcessor(bsp),
+		sdkTrace.WithResource(res),
+	)
+
+	otel.SetTracerProvider(tp)
+
+	// register the TraceContext propagator globally.
+	var tc propagation.TraceContext
+
+	otel.SetTextMapPropagator(tc)
+
+	tracer := otel.Tracer("hls/client")
+
+	return tracer
+}
