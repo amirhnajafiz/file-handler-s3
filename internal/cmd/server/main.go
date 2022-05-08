325f2abb3f8189558773b1a84e3443b9bb28b02f
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-08 15:52:37 +0430

    server init

--- internal/cmd/server/main.go
+++ internal/cmd/server/main.go
@@ -1,1 +1,26 @@
 package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func New() {
+	// configure the songs' directory name and port
+	const songsDir = "songs"
+	const port = 8080
+
+	// add a handler for the song files
+	http.Handle("/", addHeaders(http.FileServer(http.Dir(songsDir))))
+	fmt.Printf("Starting server on %v\n", port)
+	log.Printf("Serving %s on HTTP port: %v\n", songsDir, port)
+}
+
+// addHeaders will act as middleware to give us CORS support
+func addHeaders(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		h.ServeHTTP(w, r)
+	}
+}
