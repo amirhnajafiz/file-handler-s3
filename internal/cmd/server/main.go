e4a981e3cfd9fb5ee6e3c7b4703e3b2de85132d2
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-09 12:08:10 +0430

    accepting all files in uploading file

--- internal/cmd/server/main.go
+++ internal/cmd/server/main.go
@@ -2,22 +2,23 @@ package server
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"net/http"
+	"os"
 )
 
-func New(cfg Config) {
-	// configure the songs' directory name and port
-	const songsDir = "songs"
-	port := cfg.Port
+// configure the songs' directory name
+const songsDir = "songs"
 
+func New(cfg Config) {
 	// add a handler to play song files
 	http.Handle("/", addHeaders(http.FileServer(http.Dir(songsDir))))
 	// add a handler for uploading a file
 	http.Handle("/upload", uploadFile())
-	fmt.Printf("Starting server on %v\n", port)
-	log.Printf("Serving %s on HTTP port: %v\n", songsDir, port)
+	fmt.Printf("Starting server on %v\n", cfg.Port)
+	log.Printf("Serving %s on HTTP port: %v\n", songsDir, cfg.Port)
 
 	// serve and log errors
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", cfg.Port), nil))
@@ -57,5 +58,15 @@ func uploadFile() http.HandlerFunc {
 		log.Printf("Uploaded File: %+v\n", handler.Filename)
 		log.Printf("File Size: %+v\n", handler.Size)
 		log.Printf("MIME Header: %+v\n", handler.Header)
+
+		// create a temp file
+		tempFile, err := ioutil.TempFile(songsDir, "*")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		defer func(tempFile *os.File) {
+			_ = tempFile.Close()
+		}(tempFile)
 	}
 }
