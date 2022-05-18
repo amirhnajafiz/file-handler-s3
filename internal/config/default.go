e665eb24e0cc3ef87bc1c7e442c70c35b28dbc52
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-18 09:17:56 +0430

    default configs set

--- internal/config/default.go
+++ internal/config/default.go
@@ -1,5 +1,28 @@
 package config
 
+import (
+	"github.com/amirhnajafiz/hls/internal/cmd/server"
+	"github.com/amirhnajafiz/hls/internal/telemetry/config"
+)
+
 func Default() Config {
-	return Config{}
+	return Config{
+		Telemetry: config.Config{
+			Trace: config.Trace{
+				Enabled: false,
+				Agent: config.Agent{
+					Host: "",
+					Port: "",
+				},
+			},
+			Metric: config.Metric{
+				Enabled: true,
+				Address: ":1224",
+			},
+		},
+		Server: server.Config{
+			Host: "0.0.0.0",
+			Port: 8080,
+		},
+	}
 }
