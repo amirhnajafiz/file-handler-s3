9e8d6a591dead1c327a28f2e5944f60be4303e5c
amirhnajafiz <najafizadeh21@gmail.com>
2023-01-06 22:22:16 +0330

    add: s3 upload method

--- internal/port/s3/s3.go
+++ internal/port/s3/s3.go
@@ -1,7 +1,11 @@
 package s3
 
 import (
+	"io"
+
 	"github.com/amirhnajafiz/hls/internal/storage"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 // Handler contains the methods for uploading image to s3
@@ -11,8 +15,18 @@ type Handler struct {
 }
 
 // Upload file to s3 cluster.
-func (h *Handler) Upload(bytes []byte) (string, error) {
-	return "", nil
+func (h *Handler) Upload(key string, file io.Reader) error {
+	// creating a new uploader
+	uploader := s3manager.NewUploader(h.Storage.Session)
+
+	// upload image into s3 database
+	_, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(h.Storage.Cfg.Bucket),
+		Key:    aws.String(key),
+		Body:   file,
+	})
+
+	return err
 }
 
 // Download file from s3 cluster.
